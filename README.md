3c82352e9f2208d61c1f2c7e7b6862dbdea177a5
Ryan Cox <rbc@Ryans-MacBook-Pro.local>
2014-05-21 15:12:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Usage                                                                                                                                                                                
+=======
+
+For now, script assumes formatting of http://www.songlyrics.com/.  To rip lyrics just clone the repo and cd in.  Edit url var in main script, then run like this -
+
+    ./__main__.py
+
+It will write the lyrics to file in your dir, named after final_url_slug.txt.
+
---
+++ __main__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+from lxml import html
+import requests
+
+#url="http://www.songlyrics.com/mark-mulcahy/i-taketh-away-lyrics/"
+url="http://www.songlyrics.com/mark-mulcahy/the-rabbit-lyrics/"
+target='//p[@id="songLyricsDiv"]/text()'
+# remove strings like '(pre-chorus)'
+remove_paren_words=True
+remove_verse_lines=True #remove lines that start with string "verse"
+debug=True
+
+#get (part of string) between parens and remove
+def sliceOutParens(str):
+  sub_str=str[str.find("("):str.find(")")+1]
+  return str.replace(sub_str,'')
+
+# grab webpage and get the text tree
+page = requests.get(url)
+tree = html.fromstring(page.text)
+if debug: print tree
+
+slugs=url.split('/')
+filename=slugs[len(slugs)-2]+".txt"
+if debug: print "lyrics will be written to " + filename
+f=open(filename,"w")
+
+# grab desired dom element
+lyrics = tree.xpath(target)
+
+# remove punctuation chars
+for line in lyrics:
+  if remove_paren_words: line=sliceOutParens(line)
+  # come back to following in if it's a consistent issue...
+  #if remove_verse_lines and line[0:5]=='verse': break
+  line=line.replace('"','').replace('\'','').replace('.','').replace(',','').strip('\n').lower()
+  if line:
+    if debug: print line,
+    f.write(line)    
+
+f.close()

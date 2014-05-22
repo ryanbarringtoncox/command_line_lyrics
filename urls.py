dbec842e591d1313460702f3f0075b092508ab40
Ryan Cox <rbc@Ryans-MacBook-Pro.local>
2014-05-22 15:38:18 -0400

    gets urls from urls.py list

---
+++ urls.py
@@ -0,0 +1,6 @@
+# put urls to songs you want like this -
+urls = [
+  "http://www.songlyrics.com/mark-mulcahy/the-rabbit-lyrics/",
+  "http://www.songlyrics.com/rem/it-s-the-end-of-the-world-lyrics/",
+  "http://www.songlyrics.com/buddy-holly/rock-me-my-baby-lyrics/"
+]
--- word-scraper.py
+++ word-scraper.py
@@ -1,44 +1,47 @@
 #!/usr/bin/python
-from lxml import html
 import requests,sys
+from lxml import html
+from urls import urls
 
-debug=True
-url=sys.argv[1]
-if debug: print "url is " + url
-target='//p[@id="songLyricsDiv"]/text()'
-# remove strings like '(pre-chorus)'
-remove_paren_words=True
+debug=False
 
-#get (part of string) between parens and remove
-def sliceOutParens(str):
-  sub_str=str[str.find("("):str.find(")")+1]
-  return str.replace(sub_str,'')
+for url in urls:
+  if debug: print "url is " + url
+  # dom target, may change in time
+  target='//p[@id="songLyricsDiv"]/text()'
+  # remove strings like '(pre-chorus)'
+  remove_paren_words=True
 
-# grab webpage and get the text tree
-page = requests.get(url)
-tree = html.fromstring(page.text)
-if debug: print tree
+  #get (part of string) between parens and remove
+  def sliceOutParens(str):
+    sub_str=str[str.find("("):str.find(")")+1]
+    return str.replace(sub_str,'')
 
-url=url.rstrip('/')
-slugs=url.split('/')
-filename=slugs[len(slugs)-1]+".txt"
-if debug: print "lyrics will be written to " + filename
-f=open(filename,"w")
+  # grab webpage and get the text tree
+  page = requests.get(url)
+  tree = html.fromstring(page.text)
+  if debug: print tree
 
-# grab desired dom element
-lyrics = tree.xpath(target)
-if debug: print str(len(lyrics)) + " lines found in element" 
-if len(lyrics) == 0:
-  print "Invalid url.  Or url contains no words in <div id='songLyricsDiv'></div>"
-  print "  ...exiting"
-  sys.exit(2)
+  url=url.rstrip('/')
+  slugs=url.split('/')
+  filename=slugs[len(slugs)-1]+".txt"
+  if debug: print "lyrics will be written to " + filename
+  f=open(filename,"w")
 
-# remove punctuation chars
-for line in lyrics:
-  if remove_paren_words: line=sliceOutParens(line)
-  line=line.replace('"','').replace('\'','').replace('.','').replace(',','').strip('\n').lower()
-  if line:
-    if debug: print line,
-    f.write(line)    
+  # grab desired dom element
+  lyrics = tree.xpath(target)
+  if debug: print str(len(lyrics)) + " lines found in element" 
+  if len(lyrics) == 0:
+    print "Invalid url.  Or url contains no words in <div id='songLyricsDiv'></div>"
+    print "  ...exiting"
+    sys.exit(2)
 
-f.close()
+  # remove punctuation chars
+  for line in lyrics:
+    if remove_paren_words: line=sliceOutParens(line)
+    line=line.replace('"','').replace('\'','').replace('.','').replace(',','').strip('\n').lower()
+    if line:
+      if debug: print line,
+      f.write(line)    
+
+  f.close()
